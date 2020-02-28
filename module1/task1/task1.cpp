72bbc781bd7c4b5a5ca92d172489fbe99850d529
Danila Mishin <danila.mishin.2001@gmail.com>
2020-02-28 14:48:02 +0300

    Enum added

--- module1/task1/task1.cpp
+++ module1/task1/task1.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <queue>
 
+enum TEntityNameType {
+  NT_LEON,
+  NT_MATILDA,
+  NT_MILK,
+};
 
 //Находим для каждой вершины графа кол-во шагов, которое нужно пройти, чтобы дойти от вершины
 //from до этой вершины. Возвращаем вектор расстояний 
@@ -52,13 +57,18 @@ int main() {
         graph[b-1].push_back(a-1);
     }
     
-    std::vector<int> milk_distances = find_distances(graph, milkVertex);
-    std::vector<int> leo_distances = find_distances(graph, leonVertex);
-    std::vector<int> mal_distances = find_distances(graph, malVertex);
+    std::vector<std::vector<int>> entityDistance(3);
+    entityDistance[NT_LEON] = find_distances(graph, leonVertex);
+    entityDistance[NT_MATILDA] = find_distances(graph, malVertex);
+    entityDistance[NT_MILK] = find_distances(graph, milkVertex);
 
-    int min_answer = milk_distances[0] + leo_distances[0] + mal_distances[0];
-    for(int i = 1; i < vertexNum; i++) {
-        int new_answer = milk_distances[i] + leo_distances[i] + mal_distances[i];
+    int min_answer = 0;
+    for(size_t i = 0; i < entityDistance.size(); ++i) {
+        min_answer = entityDistance[NT_LEON][0] + entityDistance[NT_MATILDA][0] + entityDistance[NT_MILK][0];
+    }
+    
+    for(int i = 1; i < vertexNum; ++i) {
+        int new_answer = entityDistance[NT_LEON][i] + entityDistance[NT_MATILDA][i] + entityDistance[NT_MILK][i];
         min_answer = (min_answer < new_answer ? min_answer : new_answer);
     }
 
